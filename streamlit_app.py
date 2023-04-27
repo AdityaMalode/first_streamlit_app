884f30a8c1d9dc2e720f989a325f96a3ea1aba87
AdityaMalode <131974292+AdityaMalode@users.noreply.github.com>
2023-04-27 18:03:08 +0530

    Update streamlit_app.py
    Add header and menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Just startede with GitHub & Streamlit!')
+
+streamlit.header('Going with the Badge code for practice! Menu Card it is!')
+streamlit.text('Aloo Paratha')
+streamlit.text('Gobi Paratha')
+streamlit.text('Chicken Sandwitch')
+streamit.text('Chicken Soup')
