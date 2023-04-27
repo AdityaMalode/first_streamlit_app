f7bbe0bfe2971f9407459f63cd7f5e9e8db6cc53
AdityaMalode <131974292+AdityaMalode@users.noreply.github.com>
2023-04-27 18:10:45 +0530

    Update streamlit_app.py
    -Add panda library
    -import txt file for fruit list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🐔Chicken Sandwitch')
 streamlit.text('🐔Chicken Soup')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
