29acda16e27df1cd1a69c2bd0820a2982b16d4c4
AdityaMalode <131974292+AdityaMalode@users.noreply.github.com>
2023-05-03 19:56:43 +0530

    Update streamlit_app.py
    adding another user choice box

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# new section to display fruityvice api response
+streamlit.header("Fruit Advice!")
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','apple')
+streamlit.write('The user entered ', add_my_fruit)
