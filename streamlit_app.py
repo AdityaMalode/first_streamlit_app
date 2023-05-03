4ad49e61272db732901b47736f9c815567c02244
AdityaMalode <131974292+AdityaMalode@users.noreply.github.com>
2023-05-03 20:34:15 +0530

    Update streamlit_app.py
    added above the try if else part

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,14 +36,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-
-
 # new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -57,6 +49,16 @@ try:
 
 except URLError as e:
   streamlit.error()
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page.
+#streamlit.dataframe(fruits_to_show)
+
+
+
     
 #streamlit.write('The user entered ', fruit_choice)
 
