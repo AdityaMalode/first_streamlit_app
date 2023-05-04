f718135d6bc31123b4b9ee7b9bd44f59598459e6
AdityaMalode <131974292+AdityaMalode@users.noreply.github.com>
2023-05-05 03:24:47 +0530

    Update streamlit_app.py
    comment previous load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,12 @@ except URLError as e:
   streamlit.error()
 
   
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The Fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 streamlit.header("The Fruit load list contains:")
 #snowflake related function
