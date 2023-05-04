5f715eb1b3cb3cea5886a73eda2bc412348027f6
AdityaMalode <131974292+AdityaMalode@users.noreply.github.com>
2023-05-05 03:32:39 +0530

    Update streamlit_app.py
    added user acceptance input function

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,6 +62,19 @@ if streamlit.button('Get fruit load list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
   
+ 
+# allow end user to add new fruit
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+  my_cur.execute("Insert into fruit load list values('from streamlit')")
+  return "Thanks for adding" + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add fruit to a list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_new_fruit)
+  streamlit.text(back_from_function)
+  
 
 
 # new section to display fruityvice api response
@@ -92,7 +105,7 @@ if streamlit.button('Get fruit load list'):
 
 
 # don't run the above part as we're working on fixing it.
-streamlit.stop()
+#streamlit.stop()
 
 
 
